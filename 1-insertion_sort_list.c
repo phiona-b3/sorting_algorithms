1c2eb2331434dd0bb585d9ed9290842eca4d0fa2
Phiona Beatrice Ndemaru <ndemaruphionabeatrice@gmail.com>
2022-08-15 13:07:45 -0700

    Task 0

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm
+ * @list: double pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *head, *node, *tmp;
+
+if (list == NULL || *list == NULL)
+return;
+head = node = *list;
+while (node)
+{
+if (!node->prev)
+{
+node = node->next;
+continue;
+}
+else if (node->n < node->prev->n)
+{
+tmp = node->prev;
+tmp->next = node->next;
+if (node->next)
+node->next->prev = tmp;
+node->next = tmp;
+node->prev = tmp->prev;
+tmp->prev = node;
+if (node->prev)
+node->prev->next = node
+else
+{
+node->prev = NULL;
+head = node;
+}
+print_list(head);
+}
+else if (node->n >= node->prev->n && !node->next)
+{
+*list = head;
+return;
+}
+else
+{
+node = node->next;
+}
+}
+}
